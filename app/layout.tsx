642e5272bb3cc2fc099f18978eba3bcc327920ff
Renan Teixeira <renan_replay@live.com>
2024-09-03 15:53:35 -0300

    configure biome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,54 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Oxygen } from 'next/font/google';
+import { Oxygen } from "next/font/google";
 
-const oxygen = Oxygen({ weight: ['300', '400', '700'], subsets: ['latin'] })
+const oxygen = Oxygen({ weight: ["300", "400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Psi Camila Samogim - Psicóloga Infantil em Jundiaí',
-  description: 'Psicóloga Infantil em Jundiaí, atendimento para crianças e adolescentes, orientação de pais.',
-  alternates: {
-    canonical: `psicamilasamogim.com`,
-  },
-  icons: {
-    icon: '/icon.png',
-  },
-  manifest: 'psicamilasamogim.com/manifest.json',
-  keywords: [
-    'psicóloga infantil',
-    'psicóloga em jundiaí',
-    'psicóloga infantil em jundiaí',
-    'psicóloga para crianças',
-    'psicóloga para adolescentes',
-    'orientação de pais',
-  ],
-  openGraph: {
-    title: 'Calculadora de Churrasco - Bora Churrasco!',
-    description: 'Psicóloga Infantil em Jundiaí, atendimento para crianças e adolescentes, orientação de pais.',
-    url: `psicamilasamogim.com`,
-    images: [
-      {
-        url: '/og-image.png',
-        alt: 'Psi Camila Samogim - Psicóloga Infantil em Jundiaí',
-      },
-    ],
+	title: "Psi Camila Samogim - Psicóloga Infantil em Jundiaí",
+	description:
+		"Psicóloga Infantil em Jundiaí, atendimento para crianças e adolescentes, orientação de pais.",
+	alternates: {
+		canonical: "psicamilasamogim.com",
+	},
+	icons: {
+		icon: "/icon.png",
+	},
+	manifest: "psicamilasamogim.com/manifest.json",
+	keywords: [
+		"psicóloga infantil",
+		"psicóloga em jundiaí",
+		"psicóloga infantil em jundiaí",
+		"psicóloga para crianças",
+		"psicóloga para adolescentes",
+		"orientação de pais",
+	],
+	openGraph: {
+		title: "Calculadora de Churrasco - Bora Churrasco!",
+		description:
+			"Psicóloga Infantil em Jundiaí, atendimento para crianças e adolescentes, orientação de pais.",
+		url: "psicamilasamogim.com",
+		images: [
+			{
+				url: "/og-image.png",
+				alt: "Psi Camila Samogim - Psicóloga Infantil em Jundiaí",
+			},
+		],
 
-    locale: 'pt_BR',
-    type: 'website',
-  },
-}
+		locale: "pt_BR",
+		type: "website",
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={'' + oxygen.className}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${oxygen.className}`}>{children}</body>
+		</html>
+	);
 }
--- components/JsonLd.js
+++ components/JsonLd.js
@@ -1,8 +1,9 @@
 const JsonLd = ({ data }) => (
-  <script
-    type="application/ld+json"
-    dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-  />
+	<script
+		type="application/ld+json"
+		// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+		dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+	/>
 );
 
 export default JsonLd;
